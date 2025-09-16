90a652612dacf0410c174a31a6b511e8d3139003
Alexandre Simao <GD75Alexandre@VFS.com>
2025-09-16 16:03:03 -0700

    Got rid of compiler errors

--- Source/ElectricGuyGame/Private/World Objects/DashCrystal.cpp
+++ Source/ElectricGuyGame/Private/World Objects/DashCrystal.cpp
@@ -12,7 +12,7 @@ ADashCrystal::ADashCrystal()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	CapsuleCollider = FindComponentByClass<UCapsuleComponent>();
+	//CapsuleCollider = FindComponentByClass<UCapsuleComponent>();
 	
 }
 
--- Source/ElectricGuyGame/Public/World Objects/DashCrystal.h
+++ Source/ElectricGuyGame/Public/World Objects/DashCrystal.h
@@ -23,8 +23,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, Category = "Capsule", BlueprintReadWrite)
-	UCapsuleComponent* CapsuleCollider;
+	//UPROPERTY(EditAnywhere, Category = "Capsule", BlueprintReadWrite)
+	//UCapsuleComponent* CapsuleCollider;
 	
 public:	
 	// Called every frame
